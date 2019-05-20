fda6f70d6cd8cae62062759f565e2352102a60db
Yuri Protásio <yuriprotasio@hotmail.com>
2019-05-20 20:07:30 -0300

    adjustments to mobile and menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,4 +20,9 @@ export class HomeComponent {
 			event.target.className = ''
 		}, 1000)
 	}
+
+	scrollTo (target) {
+		let element = document.getElementById(target)
+		element.scrollIntoView({behavior:"smooth"})
+	}
 }
